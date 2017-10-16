2635c6f542d723884795977287f5b2fd4b448e49
robertjiang <robertjiang@zwdai.com>
2017-10-16 13:49:50 +0800

    json zw

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var app = express();
 
 app.get('/.well-known/assetlinks.json', function(req, res){
 	var jsonStr = [{
-				 "relation": ["delegate_permission/common.handle_all_urls"],
-				 "target": {
-					 "namespace": "android_app",
-				 	"package_name": "com.rongjing.zwdai",
-				 	"sha256_cert_fingerprints": ["9F:C6:33:7A:C0:8C:80:DF:3B:F9:01:C8:57:C3:D0:FD:00:87:FD:A6:9D:F8:21:01:99:43:E8:14:DD:A1:D0:29"]
-			 	}
-			}];
+	  "relation": ["delegate_permission/common.handle_all_urls"],
+	  "target": {
+	    "namespace": "android_app",
+	    "package_name": "com.rongjing.zwdai",
+	    "sha256_cert_fingerprints":
+	    ["9F:C6:33:7A:C0:8C:80:DF:3B:F9:01:C8:57:C3:D0:FD:00:87:FD:A6:9D:F8:21:01:99:43:E8:14:DD:A1:D0:29"]
+	  }
+	}];
 
 	res.json(jsonStr);
 });
